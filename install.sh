fb28f97d1825a088742f1c4aa4349e96913f3f02
Miguel Guthridge <hdsq@outlook.com.au>
2022-07-18 16:02:23 +1000

    Add zshrc

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 ln -s ~/.dotfiles/.bash_aliases      ~/.bash_aliases
-ln -s ~/.dotfiles/.bashrc           ~/.bashrc
+ln -s ~/.dotfiles/.bashrc            ~/.bashrc
+ln -s ~/.dotfiles/.zshrc             ~/.zshrc
 ln -s ~/.dotfiles/.inputrc           ~/.inputrc
 
 mkdir -p ~/.config/cloc
-ln -s ~/.dotfiles/cloc_options.txt ~/.config/cloc/options.txt
+ln -s ~/.dotfiles/cloc_options.txt   ~/.config/cloc/options.txt
