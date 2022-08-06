6c49490a01e678841e9c04055e00f7ba5349796f
Miguel Guthridge <hdsq@outlook.com.au>
2022-08-06 21:27:36 +1000

    Rename files

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-ln -s ~/.dotfiles/.bash_aliases      ~/.bash_aliases
-ln -s ~/.dotfiles/.bashrc            ~/.bashrc
-ln -s ~/.dotfiles/.zshrc             ~/.zshrc
-ln -s ~/.dotfiles/.zsh_aliases       ~/.zsh_aliases
-ln -s ~/.dotfiles/.inputrc           ~/.inputrc
+ln -si ~/.dotfiles/bash_aliases      ~/.bash_aliases
+ln -si ~/.dotfiles/bashrc            ~/.bashrc
+ln -si ~/.dotfiles/zshrc             ~/.zshrc
+ln -si ~/.dotfiles/zsh_aliases       ~/.zsh_aliases
+ln -si ~/.dotfiles/inputrc           ~/.inputrc
 
 mkdir -p ~/.config/cloc
-ln -s ~/.dotfiles/cloc_options.txt   ~/.config/cloc/options.txt
+ln -si ~/.dotfiles/cloc_options.txt   ~/.config/cloc/options.txt
