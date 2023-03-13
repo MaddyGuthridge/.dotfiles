c94c1ed8a1ebae6867fc83e513381579158fc237
Miguel Guthridge <hdsq@outlook.com.au>
2023-03-14 00:55:52 +1100

    Further improve RmView stuff

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ ln -si ~/.dotfiles/bashrc            ~/.bashrc
 ln -si ~/.dotfiles/zshrc             ~/.zshrc
 ln -si ~/.dotfiles/zsh_aliases       ~/.zsh_aliases
 ln -si ~/.dotfiles/inputrc           ~/.inputrc
+ln -si ~/.dotfiles/rmview/RmView.desktop ~/.local/share/applications/RmView.desktop
 
 mkdir -p ~/.config/cloc
 ln -si ~/.dotfiles/cloc_options.txt   ~/.config/cloc/options.txt
