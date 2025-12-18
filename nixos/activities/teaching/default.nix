8f3a9cd558d0f978360496eee45aa1909148ae77
Maddy Guthridge <hello@maddyguthridge.com>
2025-12-18 17:48:50 +1100

    Add v412 loopback and the like for video recording

--- nixos/activities/teaching/default.nix
+++ nixos/activities/teaching/default.nix
@@ -1,10 +1,27 @@
 { config, pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    input-remapper
+    # UML and C4 modelling
     unstable.gaphor
+    # Remote access to CSE
     tigervnc
+    # Presentations
+    input-remapper
+    # DSLR as webcam
+    gphoto2
+    ffmpeg
+    obs-studio
+    v4l-utils
   ];
+  
+  boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback.out ];
+  boot.kernelModules = [
+    # Allow creating arbitrary webcams
+    "v4l2loopback"
+  ];
+  boot.extraModprobeConfig = ''
+    options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"
+  '';
 
   # BRO WHY DOES THIS NOT WORK
   # services.input-remapper.enable = true;
