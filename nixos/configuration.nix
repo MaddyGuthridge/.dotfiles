c5fda1f957c0d5c1132477e4ebe78bce97f10a3e
Maddy Guthridge <hello@maddyguthridge.com>
2026-01-12 23:17:26 +1100

    NixOS: Enable gpg

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -192,6 +192,12 @@ in
     clean.extraArgs = "--keep-since 7d --keep 10";
   };
 
+  # GnuPG Agent
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
