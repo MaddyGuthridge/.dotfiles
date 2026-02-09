cbf126c0fe07179dce5a9d597a03bc69a0f6dc1e
Maddy Guthridge <hello@maddyguthridge.com>
2026-02-10 00:18:03 +1100

    Disable fingerprint auth for sudo and polkit

--- nixos/hardware/yzma.nix
+++ nixos/hardware/yzma.nix
@@ -39,5 +39,22 @@ in
       tod.enable = true;
       tod.driver = pkgs.libfprint-2-tod1-goodix;
     };
+
+    # Disable fingerprint for sudo and Gnome Polkit, as they mean I cannot enter a
+    # password unless I first fail the fingerprint scan 3 times. Very annoying.
+    # https://github.com/NixOS/nixpkgs/issues/171136
+    # https://discussion.fedoraproject.org/t/authenticate-with-password-in-authentication-required-prompt/86932
+    # https://gitlab.freedesktop.org/libfprint/libfprint/-/issues/403
+    # https://gitlab.gnome.org/GNOME/gnome-shell/-/issues/5445
+    #
+    # Supposedly, sudo (using fprintd) should fall back to password on SIGINT:
+    # https://gitlab.freedesktop.org/libfprint/fprintd/-/commit/657f58fd648e35417ce7266b9c1558ce497dc179
+    # But this did not work for me.
+    #
+    # This could hypothetically be fixed using this patch, but I am not brave
+    # enough to run something called "gross hack" when it comes to auth:
+    # https://gitlab.com/mishakmak/pam-fprint-grosshack
+    security.pam.services.sudo.fprintAuth = false;
+    security.pam.services.polkit-1.fprintAuth = false;
   };
 }
