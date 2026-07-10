b15ea8ee8d700386e18fb978c9fd2830e79e25e9
Maddy Guthridge <hello@maddyguthridge.com>
2026-07-10 23:31:46 +1000

    nixos: fix obsidian crash

--- nixos/activities/documents.nix
+++ nixos/activities/documents.nix
@@ -1,7 +1,9 @@
 { config, pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    unstable.obsidian
+    # Force Electron 40 for Obsidian, since Harper plugin causes a crash.
+    # https://github.com/NixOS/nixpkgs/issues/539153
+    (obsidian.override { electron = pkgs.electron_40; })
     libreoffice-qt6-fresh
     # Pinning to 25.11 due to segfaults
     # https://github.com/NixOS/nixpkgs/issues/527173
