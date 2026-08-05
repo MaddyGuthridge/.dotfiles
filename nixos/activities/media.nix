7f9ebf0b06f81924ec4f083113b38deee0107961
Maddy Guthridge <hello@maddyguthridge.com>
2026-08-05 22:43:45 +1000

    nixos: undo gstreamer plugins in system path

--- nixos/activities/media.nix
+++ nixos/activities/media.nix
@@ -40,10 +40,11 @@ in
   ];
 
   # Make gstreamer discoverable in Nautilus
-  environment.sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 = lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" [
-    pkgs.gst_all_1.gst-plugins-good
-    pkgs.gst_all_1.gst-plugins-bad
-    # pkgs.gst_all_1.gst-plugins-ugly
-    pkgs.gst_all_1.gst-libav
-  ];
+  # Never mind, this gives tons of dynamic link errors
+  # environment.sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 = lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" [
+  #   pkgs.gst_all_1.gst-plugins-good
+  #   pkgs.gst_all_1.gst-plugins-bad
+  #   # pkgs.gst_all_1.gst-plugins-ugly
+  #   pkgs.gst_all_1.gst-libav
+  # ];
 }
