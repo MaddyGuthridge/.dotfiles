d160d591497911fa3be1136f024a405417d67d1e
Maddy Guthridge <hello@maddyguthridge.com>
2026-09-16 19:08:16 +1000

    nixos: switch pinta to main channel

--- nixos/activities/documents.nix
+++ nixos/activities/documents.nix
@@ -5,9 +5,7 @@
     # https://github.com/NixOS/nixpkgs/issues/539153
     (obsidian.override { electron = pkgs.electron_40; })
     libreoffice-qt6-fresh
-    # Pinning to 25.11 due to segfaults
-    # https://github.com/NixOS/nixpkgs/issues/527173
-    nixos2511.pinta
+    pinta
     gimp
     inkscape
     krita
